c61ed440375b8341620659f3901b8873fccb9b9b
0x3phemeralsoul <m@m.com>
2023-12-08 11:01:07 +0000

    done

--- getOrdersByUid.py
+++ getOrdersByUid.py
@@ -2,7 +2,7 @@ import requests
 import os
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import dateutil.parser
 
 
@@ -34,61 +34,59 @@ def main():
 
 
     # Example: Query all transactions from the table
-
-    orders = session.query(Order.orderHash).distinct().all()
-    
+    orders_query = text("SELECT DISTINCT orderHash FROM orders where orderHash not in (SELECT DISTINCT uid FROM orders_by_uid)")
+ 
+    orders = session.execute(orders_query).all()
+    print(f"Entries to process {len(orders)}")
+    session.commit()
     # Fetch API data for each transaction
     for order in orders:
         order_hash = order.orderHash
-        exists = session.query(OrderByUid.uid).filter_by(uid=order_hash).first() is not None
-        if(not exists):
-
-            solver_competition_api_data = fetch_order_api_data(order_hash)
+        solver_competition_api_data = fetch_order_api_data(order_hash)
+        
+        if solver_competition_api_data:
+            # Process the API data as needed
+            print(f"API Data for order_hash {order_hash}")
+            order = OrderByUid(
+            creation_date = dateutil.parser.isoparse(solver_competition_api_data["creationDate"]),
+            owner = solver_competition_api_data["owner"], 
+            uid = solver_competition_api_data["uid"],
+            available_balance = solver_competition_api_data["availableBalance"],
+            executed_buy_amount = solver_competition_api_data["executedBuyAmount"],
+            executed_sell_amount = solver_competition_api_data["executedSellAmount"],
+            executed_sell_amount_before_fees = solver_competition_api_data["executedSellAmountBeforeFees"],
+            executed_fee_amount = solver_competition_api_data["executedFeeAmount"],
+            invalidated = solver_competition_api_data["invalidated"] == 'true' if True else False ,
+            status = solver_competition_api_data["status"],
+            class_ = solver_competition_api_data["class"],
+            executed_surplus_fee = solver_competition_api_data["executedSurplusfee"] if ('executedSurplusfee' in solver_competition_api_data)  else '',
+            settlement_contract = solver_competition_api_data["settlementContract"],
+            full_fee_amount = solver_competition_api_data["fullFeeAmount"],
+            solver_fee = solver_competition_api_data["solverFee"],
+            is_liquidity_order = True if solver_competition_api_data["isLiquidityOrder"] == 'true' else False ,
+            full_app_data = solver_competition_api_data["fullAppData"],
+            sell_token = solver_competition_api_data["sellToken"],
+            buy_token = solver_competition_api_data["buyToken"],
+            receiver = solver_competition_api_data["receiver"],
+            sell_amount = solver_competition_api_data["sellAmount"],
+            buy_amount = solver_competition_api_data["buyAmount"],
+            valid_to = solver_competition_api_data["validTo"],
+            app_data =solver_competition_api_data["appData"],
+            fee_amount = solver_competition_api_data["feeAmount"],
+            kind = solver_competition_api_data["kind"],
+            partially_fillable = True if solver_competition_api_data["partiallyFillable"] == 'true' else False ,
+            sell_token_balance = solver_competition_api_data["sellTokenBalance"],
+            buy_token_balance = solver_competition_api_data["buyTokenBalance"],
+            signing_scheme = solver_competition_api_data["signingScheme"],
+            signature = solver_competition_api_data["signature"],
+            interactions = solver_competition_api_data["interactions"],
+            )
+            session.add(order)
+            session.commit()
             
-            if solver_competition_api_data:
-                # Process the API data as needed
-                print(f"API Data for order_hash {order_hash}")
-                order = OrderByUid(
-                creation_date = dateutil.parser.isoparse(solver_competition_api_data["creationDate"]),
-                owner = solver_competition_api_data["owner"], 
-                uid = solver_competition_api_data["uid"],
-                available_balance = solver_competition_api_data["availableBalance"],
-                executed_buy_amount = solver_competition_api_data["executedBuyAmount"],
-                executed_sell_amount = solver_competition_api_data["executedSellAmount"],
-                executed_sell_amount_before_fees = solver_competition_api_data["executedSellAmountBeforeFees"],
-                executed_fee_amount = solver_competition_api_data["executedFeeAmount"],
-                invalidated = solver_competition_api_data["invalidated"] == 'true' if True else False ,
-                status = solver_competition_api_data["status"],
-                class_ = solver_competition_api_data["class"],
-                executed_surplus_fee = solver_competition_api_data["executedSurplusfee"] if ('executedSurplusfee' in solver_competition_api_data)  else '',
-                settlement_contract = solver_competition_api_data["settlementContract"],
-                full_fee_amount = solver_competition_api_data["fullFeeAmount"],
-                solver_fee = solver_competition_api_data["solverFee"],
-                is_liquidity_order = True if solver_competition_api_data["isLiquidityOrder"] == 'true' else False ,
-                full_app_data = solver_competition_api_data["fullAppData"],
-                sell_token = solver_competition_api_data["sellToken"],
-                buy_token = solver_competition_api_data["buyToken"],
-                receiver = solver_competition_api_data["receiver"],
-                sell_amount = solver_competition_api_data["sellAmount"],
-                buy_amount = solver_competition_api_data["buyAmount"],
-                valid_to = solver_competition_api_data["validTo"],
-                app_data =solver_competition_api_data["appData"],
-                fee_amount = solver_competition_api_data["feeAmount"],
-                kind = solver_competition_api_data["kind"],
-                partially_fillable = True if solver_competition_api_data["partiallyFillable"] == 'true' else False ,
-                sell_token_balance = solver_competition_api_data["sellTokenBalance"],
-                buy_token_balance = solver_competition_api_data["buyTokenBalance"],
-                signing_scheme = solver_competition_api_data["signingScheme"],
-                signature = solver_competition_api_data["signature"],
-                interactions = solver_competition_api_data["interactions"],
-                )
-                session.add(order)
-                session.commit()
-                
-        else:
-            print(f"Order_hash {order_hash} already in DB, skipping.")
-        # Close the session
-        session.close()
+
+    # Close the session
+    session.close()
    
 
 if __name__ == "__main__":
